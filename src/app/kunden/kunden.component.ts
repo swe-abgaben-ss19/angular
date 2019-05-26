af20cf9159d2cff3d901af76f7693cd83e665f43
tiotdev <tiotdev@tiot.de>
2019-05-27 00:40:18 +0200

    Implementiere rest

--- src/app/kunden/kunden.component.ts
+++ src/app/kunden/kunden.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-kunden',
   templateUrl: './kunden.component.html',
-  styleUrls: ['./kunden.component.sass']
+  styleUrls: ['./kunden.component.sass'],
 })
 export class KundenComponent implements OnInit {
+  kunden: any = [];
 
-  constructor() { }
+  constructor(
+    public rest: RestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.getKunden();
   }
 
+  getKunden() {
+    this.kunden = [];
+    this.rest.getKunden().subscribe((data: {}) => {
+      console.log(data);
+      this.kunden = data;
+    });
+  }
 }
---
+++ src/app/rest.service.ts
@@ -0,0 +1,47 @@
+// https://www.djamware.com/post/5b87894280aca74669894414/angular-6-httpclient-consume-restful-api-example
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RestService {
+  constructor(private http: HttpClient) {}
+
+  endpoint = 'https://localhost:8444/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+  getKunden(): Observable<any> {
+    return this.http
+      .get(this.endpoint, {
+        headers: { Authorization: 'Basic YWRtaW46cA==' },
+      })
+      .pipe(map(this.extractData));
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
